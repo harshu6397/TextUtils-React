ade60fc1d4eb486786f02d6cae85db0d99d95e8d
Harsh Kumar <harshkumarkardam258@gmail.com>
2022-07-21 17:55:43 +0530

    third commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,37 +8,51 @@ export default function TextForm(props) {
 
     const handleUppercase = () => {
         setText(text.toUpperCase())
-        props.showAlert("Converted to UPPERCASE!", "Success")
+        if(text !== '') {
+            props.showAlert("Converted to UPPERCASE!", "Success")
+        }
     }
 
     const handleLowerCase = () => {
         setText(text.toLowerCase())
+        if(text !== '') {
         props.showAlert("Converted to lowercase!", "Success")
+        }
     }
 
     const handlePunctution = () => {
         setText(text.replace(/[.,-/#!$%&;:{}=\-_`~()"']/g, ' '))
+        if(text !== '') {
         props.showAlert("Punctutions removed!", "Success")
+        }
     }
 
     const handleNewLines = () => {
         setText(text.replace(/\n/g, ' '))
+        if(text !== '') {
         props.showAlert("NewLines removed!", "Success")
+        }
     }
 
     const handleSpaces = () => {
         setText(text.replace(/\s+/g, ' '))
+        if(text !== '') {
         props.showAlert("Extra spaces removed!", "Success")
+        }
     }
     
     const handleCopyText = () => {
         navigator.clipboard.writeText(text)
+        if(text !== '') {
         props.showAlert("Copied to Clipboard!", "Success")
+        }
     }
 
     const handleClearText = () => {
         setText("");
+        if(text !== ''){
         props.showAlert("Cleared!", "Success")
+        }
     }
 
     const [text, setText] = useState('');
